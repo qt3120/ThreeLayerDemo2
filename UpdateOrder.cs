800a2d2822802f5319066094365900f12dff98d9
qt3120 <66146935+qt3120@users.noreply.github.com>
2021-02-28 11:07:22 +0700

    Form

---
+++ UpdateOrder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreeLayerDemo2
+{
+    public partial class UpdateOrder : Form
+    {
+        public UpdateOrder()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateOrder_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
